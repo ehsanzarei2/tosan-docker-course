9330b5e7549a96b14de39da8c5cbb339433bfd52
zarei <zarei@Tosan.com>
2023-01-15 22:43:29 +0330

    add spring-boot

---
+++ src/main/java/com/tsn/docker/TsnDockerCourseApplication.java
@@ -0,0 +1,13 @@
+package com.tsn.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsnDockerCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsnDockerCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tsn/docker/TsnDockerCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsn.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsnDockerCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
